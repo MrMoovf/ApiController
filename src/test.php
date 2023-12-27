e06a8a8902597be6b94ad79ea6d0a486826c4a91
MrMoovf <anderskozuch1998@gmail.com>
2023-12-27 16:22:11 +0100

    dd

---
+++ src/test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Arostech\test;
+
+class ApiControllerPackage
+{
+
+    // print test funuction
+    public static function test(){
+        echo 'test worked';
+    }
+
+   
+
+}
